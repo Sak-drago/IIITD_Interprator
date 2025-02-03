f2b827079a65bbd6afdea85e32b38ebc6570d970
Sak-drago <sakshatsachdeva@gmail.com>
2025-02-03 13:15:16 +0530

    Task Update: Fixed issue #4

---
+++ .ccls-cache/@home@sakie@Desktop@repos@IIITD_Interprator/include@program.h
@@ -0,0 +1,23 @@
+#pragma once 
+#include "../defines.h"
+#include "../library/include/linearAlloc.h"
+#include "ast.h"
+#include <unordered_map>
+#include <vector>
+
+typedef struct Environment
+{
+  LinearAllocator                         memory;
+  std::unordered_map<std::string, void*>  pointers;
+} Environment;
+
+typedef struct Program
+{
+  std::vector<Node*>  statements;
+  LinearAllocator     allocator;
+  Environment         global;
+  Environment         stack;
+  Environment*        currentEnv = &global;
+} Program;
+
+extern Program runtime;
